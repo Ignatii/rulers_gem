bc3d78614fd0947b24f82820dc14a691979624a7
Ignatiy Grigorashchenko <ignatiy.grigorashchenko@saltpepper.ru>
2019-06-06 12:15:40 +0300

    Fix error

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -2,7 +2,7 @@ module Rulers
   class Application
     def get_controller_and_action(env)
       _, cont, action, after = env["PATH_INFO"].split('/', 4)
-      _, cont, action, after =[nil, 'quotes', 'a_quote', nil] if PATH_INFO == '/'
+      _, cont, action, after =[nil, 'quotes', 'a_quote', nil] if env["PATH_INFO"] == '/'
       cont = cont.capitalize # "People"
       cont += "Controller" # "PeopleController"
       [Object.const_get(cont), action]
